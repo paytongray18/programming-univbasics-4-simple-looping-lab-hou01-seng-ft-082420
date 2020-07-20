eca97a2d4433f701d4cc7c6f44b69146ccb7b0c1
Payton Gray <paytongray18@gmail.com>
2020-07-20 19:11:37 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -7,11 +7,11 @@ def loop_message_five_times(array)
   end
 end
 
-def loop_message_n_times(array,limit)
+def loop_message_n_times(message,5)
   count = 0 
   
-  while count < limit do 
-    puts "Hello Moon."
+  while count < 5 do 
+    puts message
     count += 1 
   end
 end
